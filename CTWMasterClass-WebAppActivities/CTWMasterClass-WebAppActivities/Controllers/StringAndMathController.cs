09bb6a942820e9b66d737b27be6452bc2be9dd47
Riley Young <rileydanger@icloud.com>
2020-06-22 14:32:15 -0500

    more controller/view activities

---
+++ CTWMasterClass-WebAppActivities/CTWMasterClass-WebAppActivities/Controllers/StringAndMathController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CTWMasterClass_WebAppActivities.Controllers
+{
+    public class StringAndMathController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View(viewName: "Index", model: 0);
+        }
+        [HttpPost]
+        public ActionResult DifferenceCalc(String lastName, int age)
+        {
+            int nameLength = lastName.Length;
+            int num = Math.Abs(nameLength - age);
+            return View(viewName: "Index", model: num);
+        }
+    }
+}
