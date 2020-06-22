d35e2510f18a56fe877872c84785d0f2bf235b60
Riley Young <rileydanger@icloud.com>
2020-06-22 11:58:04 -0500

    added Views and Controllers

---
+++ CTWMasterClass-WebAppActivities/CTWMasterClass-WebAppActivities/Controllers/StringActivityController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CTWMasterClass_WebAppActivities.Controllers
+{
+    public class StringActivityController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View(viewName: "Index", model: "All starts here.");
+        }
+        [HttpPost]
+        public ActionResult SayHello(string inputString)
+        {
+            return View(viewName: "Index", model: "Hello " + inputString);
+        }
+
+    }
+}
